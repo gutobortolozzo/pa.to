871a3069b73ed0cc41714ec4ce0c0e5b80c5c536
gutobortolozzo <gutobortolozzo@gmail.com>
2017-05-17 22:09:46 -0300

    Clearer names for service register and transactional controllers

--- src/services/index.js
+++ src/services/index.js
@@ -6,6 +6,35 @@ const useragent = require('express-useragent');
 const server = express();
 server.use(useragent.express());
 
+const transactionalHandler = async (handler, request, response) => {
+
+    const transactionOptions = {
+        autocommit: false
+    };
+
+    await db.sequelize.transaction(transactionOptions, async () => {
+        await handler(request, response);
+    });
+};
+
+const registerRoute = (route, controller) => {
+
+    const controllerInterface = controller[route];
+
+    const method = controllerInterface.method.toLowerCase();
+
+    server[method](route, async (request, response, next) => {
+
+        await transactionalHandler(controllerInterface.handler, request, response);
+
+        next();
+    });
+};
+
+const registerController = (controller) => {
+    Object.keys(controller).map(route => registerRoute(route, controller));
+};
+
 const globOptions = {
     cwd : './src/services'
 };
@@ -13,27 +42,6 @@ const globOptions = {
 const controllersPaths = glob.sync("**/*Controller.js", globOptions);
 const controllers = controllersPaths.map((controller) => require(`./${controller}`));
 
-controllers.forEach(controller => {
-
-    Object.keys(controller).map(route => {
-
-        const controllerInterface = controller[route];
-
-        const method = controllerInterface.method.toLowerCase();
-
-        server[method](route, async (request, response, next) => {
-
-            const transactionOptions = {
-                autocommit: false
-            };
-
-            await db.sequelize.transaction(transactionOptions, async () => {
-                await controllerInterface.handler(request, response);
-            });
-
-            next();
-        });
-    });
-});
+controllers.forEach(registerController);
 
 module.exports = server;
